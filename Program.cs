b69fe17d9e6aec947ea2c5ce9d61a17d4e360c52
Margustova <shchennikova.ma@gmail.com>
2022-11-19 14:23:46 +0200

    hw1

--- Program.cs
+++ Program.cs
@@ -122,4 +122,31 @@
 // }
 // Console.WriteLine(Triangle(a , b, c));
  
- 
+//  Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// решаем без массивов через счетчик, проверяя, что число больше 0
+
+int PositiveDigit(int m)
+{
+    int count =0;
+    int num;
+    for (int dig=0; dig <m; dig++)
+    {
+        Console.Write("Введите число: ");
+        num = Convert.ToInt32(Console.ReadLine());
+        if(num > 0)
+        count++;
+    }
+    return count;
+}
+Console.Write("Сколько чисел: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write(PositiveDigit(m));
+
+
+// 1, -7, 567, 89, 223-> 3
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
